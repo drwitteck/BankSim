fe931344f19ae1017891ef306bea51189dabc629
Derek Witteck <drwitteck@gmail.com>
2017-10-05 22:55:11 -0400

    Don't remember what I changed. Must not have been that important

--- src/edu/temple/cis/c3238/banksim/Runnable.java
+++
@@ -1,5 +0,0 @@
-package edu.temple.cis.c3238.banksim;
-
-public interface Runnable {
-    void run();
-}
--- src/edu/temple/cis/c3238/banksim/TransferThread.java
+++ src/edu/temple/cis/c3238/banksim/TransferThread.java
@@ -3,21 +3,21 @@ package edu.temple.cis.c3238.banksim;
  * @author Cay Horstmann
  * @author Modified by Paul Wolfgang
  */
-class TransferThread extends Thread implements Runnable {
+class TransferThread extends Thread {
 
     private final Bank bank;
     private final int fromAccount;
     private final int maxAmount;
 
-    public TransferThread(Bank derekBank, int from, int max) {
-        bank = derekBank;
+    public TransferThread(Bank b, int from, int max) {
+        bank = b;
         fromAccount = from;
         maxAmount = max;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; bank.isOpen() && i < 10000; i++) {
             int toAccount = (int) (bank.size() * Math.random());
             int amount = (int) (maxAmount * Math.random());
             bank.transfer(fromAccount, toAccount, amount);
